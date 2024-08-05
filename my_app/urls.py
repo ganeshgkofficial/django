b2b84cf369667cec17dcd018ba0df7eaa853835d
ganeshgkofficial <ganeshgkofficial@gmail.com>
2024-08-05 15:17:20 +0530

    django

---
+++ my_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from my_app import views
+
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('index',views.index,name="index"),
+    path('kai',views.kai,name="kai")
+]
