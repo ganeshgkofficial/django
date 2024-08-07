f40e35d3a55dda4c17d9617b09212f25717bb784
ganeshgkofficial <ganeshgkofficial@gmail.com>
2024-08-07 16:49:07 +0530

    django

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from my_app import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('index',views.index,name="index"),
+    path('test',views.home,name="home"),
+    path('',views.index,name="index"),
     path('kai',views.kai,name="kai"),
-    path('food',views.Food_add,name="food")
+    path('food',views.Food_add,name="food"),
+    path('viewfood',views.food_view,name="viewfood"),
+    path('food_delete/<int:id>/',views.food_delete,name= 'food_delete'),
+    path('food_update/<int:id>/',views.food_update,name= 'food_update')
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from my_app.forms import food_forms
+from my_app.models import Food
 
 
 # Create your views here.
@@ -18,5 +19,29 @@ def kai(request):
 
 def Food_add(request):
     form = food_forms()
-    print(form)
+    if request.method == 'POST':
+        data = food_forms(request.POST)
+        if data.is_valid():
+            data.save()
     return render(request,"food.html",{'form':form})
+
+#view
+def food_view(request):
+    data = Food.objects.all()
+    return render(request, "view_data.html",{'data': data})
+
+#delete
+def food_delete(request,id):
+    data = Food.objects.get(id=id)
+    data.delete()
+    return redirect("viewfood")
+
+#update
+def food_update(request,id):
+    data = Food.objects.get(id=id)
+    form = food_forms(instance = data)
+    if request.method == 'POST':
+        data = food_forms(request.POST,instance=data)
+        if data.is_valid():
+            data.save()
+    return render(request,"update.html",{'form':form})
